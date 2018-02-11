0c2e2ad57e593809a10e540806b5a2b5df782685
Dan Austin <dja470@bham.ac.uk>
2018-02-11 16:05:22 +0000

    Add icon

--- Digital Monitor/AppDelegate.swift
+++ Digital Monitor/AppDelegate.swift
@@ -58,6 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        DigitalMonitorAPI.sharedInstance.oauth2ClientCredentials.forgetTokens();
         DigitalMonitorAPI.sharedInstance.oauth2ClientCredentials.authorize { (json, error) in
             if let error = error {
                 print(error)
